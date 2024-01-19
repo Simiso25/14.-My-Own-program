8f464183c7ca2134a05f25185895073cf1768a45
Lenovo-User <Lenovo-User@DESKTOP-74N9OPR>
2024-01-19 08:19:20 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+        Dim surname As String
+        Dim age As Integer
+
+        name = (TextBox1.Text)
+        surname = (TextBox2.Text)
+        age = Val(TextBox3.Text)
+
+
+        If age < 18 Then
+            MessageBox.Show("You do not qualify for voting")
+
+        Else
+            MessageBox.Show("You qualify for voting")
+        End If
+
+    End Sub
+End Class
